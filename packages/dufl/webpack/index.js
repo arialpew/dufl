713647a28f9c55f3da0a7762f8d817ff276b98f7
Meillet Robin <robin.meillet@epitech.eu>
2018-11-21 06:44:05 +0100

    Remove Webpack base config from barrel

--- packages/dufl/webpack/index.js
+++ packages/dufl/webpack/index.js
@@ -1,12 +1,10 @@
 'use strict';
 
-const base = require('./base');
 const css = require('./css');
 const eslint = require('./eslint');
 const terser = require('./terser');
 
 module.exports = {
-  base,
   css,
   eslint,
   terser,
