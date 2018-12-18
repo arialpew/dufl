94e842edff42fe7419a9375ab669ab18e7aabbd2
Meillet Robin <robin.meillet@epitech.eu>
2018-12-18 23:05:06 +0100

    Add pooling in watch command

--- packages/dufl/commands/watch.js
+++ packages/dufl/commands/watch.js
@@ -42,7 +42,7 @@ module.exports = ({
 
       const compiler = webpack(webpackConfig);
 
-      const watcher = compiler.watch({}, (err, stats) => {
+      const watcher = compiler.watch({ poll: true }, (err, stats) => {
         let messages;
 
         if (err) {
